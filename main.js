87b6dc19a844f803139a3509336cb2f98d700b91
rizkyy20 <muhammadrizkyyamin@gmail.com>
2024-08-23 18:59:03 +0800

    baru ada ide

---
+++ main.js
@@ -0,0 +1,19 @@
+function ig() {
+    window.location.href = "https://instagram.com/rzkyyy.20"
+}
+
+function link() {
+    window.location.href = "https://linkedin.com/in/Muhammad-Rizky-Yamin"
+}
+
+function wa() {
+    window.location.href = "https://wa.me/+6289521485013"
+}
+
+function tiktok() {
+    window.location.href = "https://www.tiktok.com/@froz208"
+}
+
+function github() {
+    window.location.href = "https://github.com/rizkyy20"
+}
